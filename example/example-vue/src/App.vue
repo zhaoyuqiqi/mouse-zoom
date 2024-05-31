c56da0555cca85e535493595df5aa061b73bd508
zhao77 <1940988349@qq.com>
2024-05-31 10:35:29 +0800

    feat: 添加vue示例

---
+++ example/example-vue/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { VueMouseZoom } from 'vue-mouse-zoom';
+const zoom = ref(1);
+const onZoomUpdate = (scale: number) => {
+  // console.log(scale);
+  // zoom.value = scale;
+};
+</script>
+
+<template>
+  <div style="width: 300px; height: 300px; background-color: blanchedalmond">
+    <VueMouseZoom
+      :zoom="0.4"
+      :is-draggable="true"
+      @onZoomUpdate="onZoomUpdate"
+    >
+      <img
+        src="https://avatars.githubusercontent.com/u/68246760?v=4"
+        style="width: 30px; height: 200px"
+      />
+    </VueMouseZoom>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ example/example-vue/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import "vue-mouse-zoom/style"
+createApp(App).mount('#app')
